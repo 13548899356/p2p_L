a95fbd7568e87d597bfe6f0181f92ad5f29d286d
13548899356 <1798536561@qq.com>
2019-12-14 20:31:24 +0800

    后台代码

---
+++ src/main/java/com/zking/ssm/services/impl/IdentityAuditServiceImpl.java
@@ -0,0 +1,44 @@
+package com.zking.ssm.services.impl;
+
+import com.zking.ssm.mapper.IdentityAuditMapper;
+import com.zking.ssm.model.IdentityAudit;
+import com.zking.ssm.services.IdentityAuditService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class IdentityAuditServiceImpl implements IdentityAuditService {
+
+    @Autowired
+    private IdentityAuditMapper identityAuditMapper;
+
+    @Override
+    public int deleteByPrimaryKey(String id) {
+        return identityAuditMapper.deleteByPrimaryKey(id);
+    }
+
+    @Override
+    public int insert(IdentityAudit record) {
+        return identityAuditMapper.insert(record);
+    }
+
+    @Override
+    public int insertSelective(IdentityAudit record) {
+        return identityAuditMapper.insertSelective(record);
+    }
+
+    @Override
+    public IdentityAudit selectByPrimaryKey(String id) {
+        return identityAuditMapper.selectByPrimaryKey(id);
+    }
+
+    @Override
+    public int updateByPrimaryKeySelective(IdentityAudit record) {
+        return identityAuditMapper.updateByPrimaryKeySelective(record);
+    }
+
+    @Override
+    public int updateByPrimaryKey(IdentityAudit record) {
+        return identityAuditMapper.updateByPrimaryKey(record);
+    }
+}
