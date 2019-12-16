8555aec53800f4162e1e77275c96ca51d62fe4c3
13548899356 <1798536561@qq.com>
2019-12-16 10:47:30 +0800

    后台代码

---
+++ src/main/java/com/zking/ssm/services/IProductService.java
@@ -0,0 +1,20 @@
+package com.zking.ssm.services;
+
+import com.zking.ssm.model.Product;
+import com.zking.ssm.util.PageBean;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+
+public interface IProductService {
+
+List<Product> getListProduct(Product product, PageBean pageBean);
+
+
+    int addProduct(Product product);
+
+    int delProduct(Product product);
+
+    int updateProduct(Product product);
+}
