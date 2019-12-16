47a28a39a5a5fff72da6df9cbc34a848153d3751
13548899356 <1798536561@qq.com>
2019-12-16 10:47:10 +0800

    后台代码

---
+++ src/main/java/com/zking/ssm/mapper/ProductMapper.java
@@ -0,0 +1,19 @@
+package com.zking.ssm.mapper;
+
+import com.zking.ssm.model.Product;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Transactional
+public interface ProductMapper {
+
+List<Product> getListProduct(Product product);
+
+int addProduct(Product product);
+
+int delProduct(Product product);
+
+int updateProduct(Product product);
+
+}
