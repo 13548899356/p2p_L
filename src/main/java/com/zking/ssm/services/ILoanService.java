c2961c43ed0122bd4516508f63d085c5fc2c329f
Xia-Fen <1776556966@qq.com>
2019-12-17 15:08:25 +0800

    xf

---
+++ src/main/java/com/zking/ssm/services/ILoanService.java
@@ -0,0 +1,20 @@
+package com.zking.ssm.services;
+
+import com.zking.ssm.model.Loan;
+import com.zking.ssm.util.PageBean;
+
+import java.util.List;
+
+public interface ILoanService {
+
+    int deleteByPrimaryKey(String id);
+
+    int insert(Loan record);
+
+    Loan selectByPrimaryKey(String id);
+
+    int updateByPrimaryKeySelective(Loan record);
+
+    List<Loan> ListLoan(Loan loan, PageBean pageBean);
+
+}
