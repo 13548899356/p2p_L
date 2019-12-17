b10f1e14efd0cfc5ab8c7cc39fb8f033289e0225
Xia-Fen <1776556966@qq.com>
2019-12-17 15:10:26 +0800

    xf

---
+++ src/main/java/com/zking/ssm/services/impl/LoanServiceImpl.java
@@ -0,0 +1,42 @@
+package com.zking.ssm.services.impl;
+
+import com.zking.ssm.mapper.LoanMapper;
+import com.zking.ssm.model.Loan;
+import com.zking.ssm.services.ILoanService;
+import com.zking.ssm.util.PageBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class LoanServiceImpl implements ILoanService {
+
+    @Autowired
+    private LoanMapper loanMapper;
+
+    @Override
+    public int deleteByPrimaryKey(String id) {
+        return loanMapper.deleteByPrimaryKey(id);
+    }
+
+    @Override
+    public int insert(Loan record) {
+        return loanMapper.insert(record);
+    }
+
+    @Override
+    public Loan selectByPrimaryKey(String id) {
+        return loanMapper.selectByPrimaryKey(id);
+    }
+
+    @Override
+    public int updateByPrimaryKeySelective(Loan record) {
+        return loanMapper.updateByPrimaryKeySelective(record);
+    }
+
+    @Override
+    public List<Loan> ListLoan(Loan loan, PageBean pageBean) {
+        return loanMapper.ListLoan(loan);
+    }
+}
