60c04a4fd7a3c50914c5a403ff7c173da0ccd5a7
13548899356 <1798536561@qq.com>
2019-12-18 09:14:56 +0800

    后台代码

--- src/main/java/com/zking/ssm/controller/back/audit/LoanController.java
+++ src/main/java/com/zking/ssm/controller/back/audit/LoanController.java
@@ -9,6 +9,7 @@ import com.zking.ssm.services.ILoanService;
 import com.zking.ssm.services.IProductService;
 import com.zking.ssm.services.ISysUserService;
 import com.zking.ssm.util.PageBean;
+import com.zking.ssm.util.uuidUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -70,5 +71,47 @@ public class LoanController {
         return  jsonData.toJsonObject(true,dictionaries);
     }
 
+
+    //添加贷款认证
+    @RequestMapping("/addLoan")
+    @ResponseBody
+    public Object addLoan(Loan loan){
+        loan.setId(uuidUtil.getUUID());
+        int n = loanService.insert(loan);
+        if(n>0){
+            return jsonData.toJsonMessage("贷款项目审核提交成功",true);
+        }else{
+            return jsonData.toJsonMessage("",false);
+        }
+
+    }
+
+//查询该用户所认证的项目
+    @RequestMapping("/lookLoanByUser")
+    @ResponseBody
+    public Object lookLoanByUser(Loan loan){
+        List<Loan> loans = loanService.ListLoan(loan, null);
+        if(loans.size()!=0){
+            return jsonData.toJsonObject(true,loans);
+        }else{
+            return jsonData.toJsonMessage("",false);
+        }
+    }
+
+    //审核失败
+    @RequestMapping("/Update")
+    @ResponseBody
+    public Object Update(Loan loan){
+        int update = loanService.Update(loan);
+        return jsonData.toJsonObject(true,update);
+    }
+
+    //审核成功
+    @RequestMapping("/updatecg")
+    @ResponseBody
+    public Object updatecg(Loan loan){
+        int update = loanService.Updatecg(loan);
+        return jsonData.toJsonObject(true,update);
+    }
 }
 
